65fc952e7ed48d2d10a0b2a14d2cbcbb51d6510f
Travis Michette <tmichett@redhat.com>
2024-01-19 20:10:10 -0500

    Updated with new CSS for new ePub formats

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 Red Hat eBook Conversion with Calibre
 
 ## Usage
+
+Place files in the same directory as the downloaded ePub files or place the files and make them executable in the $PATH. Execute the script and enter the entire filename of the ePub file including the extension (per the prompt). Enter the name of the output PDF file (per the prompt).
+
+```
+$ ./epub_to_pdf.zsh
+Please enter the ePub Filename to convert (with extension):
+RH403_VT_Test.epub
+Please enter the name for the PDF file (with extension):
+RH403_VT_Test.pdf
+RH403_VT_Test.epub
+RH403_VT_Test.pdf
+Conversion options changed from defaults:
+  pdf_page_margin_right: 35.0
+
+... OUTPUT OMITTED ...
+
+Removed 1226 duplicate images
+100% Updated metadata in PDF
+PDF output written to /Users/travis/Downloads/RH403_VT_Test2.pdf
+Output saved to   /Users/travis/Downloads/RH403_VT_Test2.pdf
+
+```
--- epub_to_pdf.ps1
+++ epub_to_pdf.ps1
@@ -5,4 +5,6 @@ $pdf_file = Read-Host "Please enter the name for the PDF file (with extension): 
 Write-Host  $epub_file
 Write-Host $pdf_file
 
-Invoke-Expression "ebook-convert $epub_file $pdf_file --pdf-default-font-size 12 --pdf-mono-font-size 14 --pdf-page-margin-left 35 --pdf-page-margin-right 35 --pdf-page-margin-top 35 --pdf-page-margin-bottom 35 --extra-css 'pre {background-color: lightgray;}'"
+Invoke-Expression "ebook-convert $epub_file $pdf_file --pdf-default-font-size 12 --pdf-mono-font-size 14 --pdf-page-margin-left 35 --pdf-page-margin-right 35 --pdf-page-margin-top 35 --pdf-page-margin-bottom 35 --extra-css 'body { background: #fff; color: black;} kbd {background-color: #F7F7F7; border: 1px solid #BEBEBC;} pre {background: #d3d5d4;} aside.note::before {color: #004080;} aside.important::before {color: #795600;} b.button .label {background-color: #F7F7F7; border: 1px solid #BEBEBC;} h1 span.title-val {color: black; -webkit-text-fill-color: black;} h2 span.title {color: black; -webkit-text-fill-color: black;} h2 span.title-val {color: black; -webkit-text-fill-color: black;} #colophon h1.chapter-title {color: black;-webkit-text-fill-color: black;} #colophon > p:last-of-type {color: #EE0000;} table.table thead {background-color: #EE0000;} strong.contribs {margin-left: 3em; color: black; -webkit-text-fill-color: black;} body a:link[href^="mailto:"] {color: blue;} pre {background-color: lightgray;}'"
+
+
